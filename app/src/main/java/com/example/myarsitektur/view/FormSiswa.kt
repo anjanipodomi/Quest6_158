fbb68a87a8bb53d9211c309faec585450af395b5
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-21 16:00:09 +0700

    Menambahkan OutlinedTextField untuk input alamat lengkap dengan lebar 250dp dan logika onValueChange untuk menyimpan perubahan teks

--- app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
@@ -64,6 +64,17 @@ fun FormSiswa(
                 thickness = 1.dp,
                 color=Color.Red
             )
+            OutlinedTextField(
+                value = txtAlamat,
+                singleLine = true,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.width(width = 250.dp),
+                label = { Text(text = "Alamat Lengkap") },
+                onValueChange = {
+                    txtAlamat = it
+                }
+            )
+
 
 
 
