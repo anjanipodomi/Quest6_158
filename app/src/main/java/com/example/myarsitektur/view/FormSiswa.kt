8e90554f02543c4c139257e484a1aa2651b61fbd
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-21 15:58:17 +0700

    Menambahkan HorizontalDivider dengan lebar 250dp, padding 20dp, dan garis berwarna merah sebagai pemisah tampilan

--- app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/FormSiswa.kt
@@ -38,6 +38,11 @@ fun FormSiswa(
                 onValueChange = {
                     txtNama = it
                 }
+            )
+            HorizontalDivider(modifier = Modifier
+                .padding(all=20.dp)
+                .width(width=250.dp), thickness = 1.dp, color=Color.Red)
+
         }
     }
 
