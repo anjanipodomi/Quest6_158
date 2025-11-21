3440fe6041fa24f035396a03cfe27f5ad13b5e0e
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-21 14:49:54 +0700

    Menambahkan pemanggilan SiswaApp() di dalam setContent agar tampilan utama aplikasi bisa muncul di MainActivity

--- app/src/main/java/com/example/myarsitektur/MainActivity.kt
+++ app/src/main/java/com/example/myarsitektur/MainActivity.kt
@@ -1,17 +1,6 @@
 package com.example.myarsitektur
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myarsitektur.ui.theme.MyArsitekturTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +8,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyArsitekturTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SiswaApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyArsitekturTheme {
-        Greeting("Android")
     }
 }
