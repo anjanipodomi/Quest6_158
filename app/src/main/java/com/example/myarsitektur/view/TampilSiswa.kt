74d51ef5e242881a02c1ad0907c8b44de60d80df
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-21 16:16:16 +0700

    Menambahkan import yang dibutuhkan untuk TampilSiswa, termasuk layout, Material3, TopAppBarDefaults, font, unit dp/sp, color, string/dimension resources, dan model Siswa

--- app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
@@ -1,5 +1,21 @@
 package com.example.myarsitektur.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myarsitektur.R
+import com.example.myarsitektur.model.Siswa
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -50,8 +66,6 @@ fun TampilSiswa(
                 ) {
                     Text(text = stringResource(id = R.string.back))
                 }
-
-
             }
         }
     }
