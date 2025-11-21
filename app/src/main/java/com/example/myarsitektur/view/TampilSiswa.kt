f34bdddad156c35d524605fc5daf98f2ff084258
anjanipodomi <anjanidihapsari@gmail.com>
2025-11-21 16:14:02 +0700

    Menambahkan loop untuk menampilkan tiap pasangan label dan nilai siswa di Column, dengan styling teks dan HorizontalDivider berwarna cyan sebagai pemisah

--- app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
@@ -29,8 +29,22 @@ fun TampilSiswa(
         ) {
             Column(
                 modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(id =  R.dimen.padding_small))
-            )
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+            ) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(
+                            text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive,
+                            fontSize = 22.sp
+                        )
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+
+            }
         }
     }
 }
